85eb7a819d80c9879f7e3bfd2257d375dcb6ea4f
Suvarna2025 <suvarnaravi20@gmail.com>
2023-11-02 17:29:51 +0530

    completed fully

--- script.js
+++ script.js
@@ -20,48 +20,38 @@ console.log(areObjectsEqual(obj1, obj2));
 //Output:
 false
 
-//day 3 - question2
+
+// question2
 //use the rest countries api url and display all the country flag in console
 
-//1st step : create a XHR object
 var request = new XMLHttpRequest();
-//2nd step: API URL
-//.open method
-request.open("GET","https://restcountries.com/v2/all");
-//3rd step : establishing the bridge
+request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
-//4th step : once the data sucessfully received from server
-//onload is an event
-
 request.onload=function(){
     var result=JSON.parse(request.response);
     for(let i = 0 ; i< result.length ; i++){
-        console.log(result[i].flags);   
+        console.log(result[i].flag);   
 
 }
 }
 
-//day 3- question 3
+
+//question 3
 //use rest countries api url and print all countries names,regions,subregion and populations
 
-//1st step : create a XHR object
-var request = new XMLHttpRequest();
-//2nd step: API URL
-//.open method
-//2 parameters
-request.open("GET","https://restcountries.com/v2/all");
-//3rd step : establishing the bridge
-request.send();
-//4th step : once the data sucessfully received from server
-//onload is an event
-//when we use :  once the data / server is showing 200 sataus code
-request.onload=function(){
-    var result=JSON.parse(request.response);
-    for(let i = 0 ; i< result.length ; i++){
-        console.log(result[i].name);
-        console.log(result[i].region);
-        console.log(result[i].subregion); 
-        console.log(result[i].population);    
+var req = new XMLHttpRequest();
+
+req.open("GET","https://restcountries.com/v3.1/all");
+
+req.send();
+
+req.onload=function(){
+    var resu=JSON.parse(req.response);
+    for(let i = 0 ; i< resu.length ; i++){
+        console.log(resu[i].name);
+        console.log(resu[i].region);
+        console.log(resu[i].subregion); 
+        console.log(resu[i].population);    
 
 }
 }
